7992edec8d3da4e4fc027fa9485e3f7e54dab367
Hugh Brown <hughdbrown@yahoo.com>
2025-08-22 17:16:47 -0600

    Task 4 - Structure the auth service for testing
    Add modules routes and helper to main
    Added error message
    Change to port 8000 (Recently launched app is running on port 3000)
    Set up tests in auth-service
        mkdir -p tests/api
        touch tests/api/{helpers.rs,main.rs,routes.rs}
        vim tests/api/main.rs
    
    Add lib.rs
    Modify main.rs

---
+++ auth-service/src/lib.rs
@@ -0,0 +1,54 @@
+use std::error::Error;
+
+use axum::{
+    serve::Serve,
+    Router,
+    // response::Html,
+    // routing::get,
+};
+use tower_http::services::ServeDir;
+
+// This struct encapsulates our application-related logic.
+pub struct Application {
+    server: Serve<Router, Router>,
+    // address is exposed as a public field
+    // so we have access to it in tests.
+    pub address: String,
+}
+
+impl Application {
+    pub async fn build(
+        server_addr: &str,
+        server_port: i32,
+    ) -> Result<Self, Box<dyn Error>> {
+        // Here we are using ip 0.0.0.0 so the service is listening on all the configured network interfaces.
+        // This is needed for Docker to work, which we will add later on.
+        // See: https://stackoverflow.com/questions/39525820/docker-port-forwarding-not-working
+        let address = format!("{server_addr}:{server_port}");
+
+        // Move the Router definition from `main.rs` to here.
+        let router = Router::new()
+            .nest_service("/", ServeDir::new("assets"));
+
+        // Application is running on port 3000:
+        // ❯ sudo lsof -i :3000
+        // COMMAND    PID      USER   FD   TYPE             DEVICE SIZE/OFF NODE NAME
+        // com.docke 6606 hughbrown  174u  IPv6 0xa415fefe91b050ea      0t0  TCP *:hbci (LISTEN)
+        // > ps aux | grep 6606
+        // hughbrown         6606   ... /Applications/Docker.app/Contents/MacOS/com.docker.backend services --autostart
+        let listener = tokio::net::TcpListener::bind(&address)
+            .await
+            .expect(&format!("Could not bind to address {address}\nRun `sudo lsof -i :{server_port}` to find app\n"));
+
+        let address = listener.local_addr()?.to_string();
+        let server = axum::serve(listener, router);
+
+        // Create a new Application instance and return it
+        Ok(Application { server, address })
+    }
+
+    pub async fn run(self) -> Result<(), std::io::Error> {
+        println!("listening on {}", &self.address);
+        self.server.await
+    }
+}
--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -1,22 +1,12 @@
-use axum::{response::Html, routing::get, Router};
-use tower_http::services::ServeDir;
+use auth_service::Application;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .nest_service("/", ServeDir::new("assets"))
-        .route("/hello", get(hello_handler));
+    let server_addr = "0.0.0.0";
+    let server_port = 8000;
+    let app = Application::build(server_addr, server_port)
+        .await
+        .expect("Failed to build app");
 
-    // Here we are using ip 0.0.0.0 so the service is listening on all the configured network interfaces.
-    // This is needed for Docker to work, which we will add later on.
-    // See: https://stackoverflow.com/questions/39525820/docker-port-forwarding-not-working
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    println!("listening on {}", listener.local_addr().unwrap());
-
-    axum::serve(listener, app).await.unwrap();
-}
-
-async fn hello_handler() -> Html<&'static str> {
-    // TODO: Update this to a custom message!
-    Html("<h1>Hello, World!</h1>")
+    app.run().await.expect("Failed to run app");
 }
