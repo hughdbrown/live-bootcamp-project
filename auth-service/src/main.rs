237ced21f63f1cee6228e8227f171e0daebb7949
Hugh Brown <hughdbrown@yahoo.com>
2025-08-27 14:55:04 -0600

    Restore src/main.rs

---
+++ auth-service/src/main.rs
@@ -0,0 +1,9 @@
+use auth_service::Application;
+
+#[tokio::main]
+async fn main() {
+    let app = Application::build("0.0.0.0", 8000)
+        .await
+        .expect("Failed to build app");
+    app.run().await.expect("Failed to run app");
+}
