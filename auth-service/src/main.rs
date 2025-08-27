5bc6f9594159b29d84ec5d909ee2b326297cd654
Hugh Brown <hughdbrown@yahoo.com>
2025-08-27 12:36:38 -0600

    Task 5 - Implement routes using TDD
    Add reqwest to Cargo.toml as a dev dependency
    Add TestApp
    Added serde dependencies
    Add routes to server
    Move code to tests/api/routes.rs
    Update Cargo.lock

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -2,11 +2,12 @@ use auth_service::Application;
 
 #[tokio::main]
 async fn main() {
-    let server_addr = "0.0.0.0";
-    let server_port = 8000;
-    let app = Application::build(server_addr, server_port)
+    let server_addr: &str = "0.0.0.0";
+    let server_port: i32 = 8000;
+    let app: Application = Application::build(server_addr, server_port)
         .await
         .expect("Failed to build app");
-
-    app.run().await.expect("Failed to run app");
+    app.run()
+        .await
+        .expect("Failed to run app");
 }
---
+++ auth-service/src/routes/signup.rs
@@ -0,0 +1,21 @@
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+    Json,
+};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct SignupRequest {
+    pub email: String,
+    pub password: String,
+    #[serde(rename = "requires2FA")]
+    pub requires_2fa: bool,
+}
+
+pub async fn signup(
+    Json(_request): Json<SignupRequest>, // _request is unused
+) -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
